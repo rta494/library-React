65938a67a88a98b512be1a1c330b92ad79272add
rta494 <rta494@icloud.com>
2025-10-21 08:23:05 -0700

    cart update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-
 import Nav from "./components/Nav.jsx";
 import Footer from "./components/Footer.jsx";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Books from "./pages/Books.jsx";
 import { books } from "./data.js";
@@ -9,13 +8,26 @@ import BookInfo from "./pages/BookInfo.jsx";
 import Cart from "./pages/Cart.jsx";
 import React, { useState, useEffect } from "react";
 
-
 function App() {
   const [cart, setCart] = useState([]);
 
-
   function addToCart(book) {
-    setCart([ ...cart, book ])
+    setCart([...cart, { ...book, quantity: 1 }]);
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map((item) => {
+        if (item.id === book.id) {
+          return {
+            ...item,
+            quantity: quantity,
+          };
+        } else {
+          return item;
+        }
+      })
+    );
   }
 
   useEffect(() => {
@@ -24,16 +36,26 @@ function App() {
 
   return (
     <Router>
-    <div className="App">
-      <Nav />
-      <Routes>
-      <Route path='/' exact element={<Home />}/>
-      <Route path='/books' exact element={<Books books={books} />} />
-      <Route path='/books/:id' element={<BookInfo books={books} addToCart={addToCart} />} />
-      <Route path='/cart' element={<Cart books={books} />} />
-      </Routes>
-      <Footer />
-    </div>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/books" exact element={<Books books={books} />} />
+          <Route
+            path="/books/:id"
+            element={
+              <BookInfo
+                books={books}
+                addToCart={addToCart}
+                cart={cart}
+                changeQuantity={changeQuantity}
+              />
+            }
+          />
+          <Route path="/cart" element={<Cart books={books} />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 }
